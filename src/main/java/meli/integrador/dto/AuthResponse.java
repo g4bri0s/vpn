3d7c96a42c6780cc7d6258ad9dce10019e3a73b7
g4bri0s <g.cristianods@gmail.com>
2025-06-08 16:21:00 -0300

    teste2

---
+++ src/main/java/meli/integrador/dto/AuthResponse.java
@@ -0,0 +1,14 @@
+package meli.integrador.dto;
+
+import lombok.Data;
+
+@Data
+public class AuthResponse {
+    private String token;
+    private String role;
+
+    public AuthResponse(String token, String role) {
+        this.token = token;
+        this.role = role;
+    }
+}
